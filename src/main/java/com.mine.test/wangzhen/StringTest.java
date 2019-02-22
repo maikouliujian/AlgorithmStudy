ae9b3fc4b4aabb12e9ab14c7e9fbb57a93368735
刘剑 <liujian9009@126.com>
2019-02-22 19:31:11 +0800

    string

---
+++ src/main/java/com.mine.test/wangzhen/StringTest.java
@@ -0,0 +1,33 @@
+package com.mine.test.wangzhen;
+
+public class StringTest {
+
+    public static void main(String[] args) {
+        String source = "ajfasjlsabao";
+        String pattern = "aof";
+        System.out.println(BruteForce(source,pattern));
+
+    }
+
+
+    public static int BruteForce(String source,String pattern){
+        int sourcelen = source.length();
+        int patternlen = pattern.length();
+        for (int i = 0; i < sourcelen; i++) {
+            for (int j = 0; j <patternlen; j++) {
+                if (source.charAt(i +j) == pattern.charAt(j)){
+                    if (j == patternlen-1){
+                        return i;
+                    }else {
+                        continue;
+                    }
+                }else {
+                    break;
+                }
+            }
+        }
+        return -1;
+    }
+
+
+}
