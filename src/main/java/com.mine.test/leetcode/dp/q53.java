62968ad925fedce970620ed87234832f683893be
刘剑 <liujian@17shanyuan.com>
2019-08-20 15:11:09 +0800

    dp 121

--- src/main/java/com.mine.test/leetcode/dp/q53.java
+++ src/main/java/com.mine.test/leetcode/dp/q53.java
@@ -47,6 +47,19 @@ public class q53 {
             }
             return max;
         }
+
+        /***
+         * python解法
+         *
+         * def max_subarray(A):
+         *     max_ending_here = max_so_far = A[0]
+         *     for x in A[1:]:
+         *         max_ending_here = max(x, max_ending_here + x)
+         *         max_so_far = max(max_so_far, max_ending_here)
+         *     return max_so_far
+         */
+
+
     }
 
 }
---
+++ src/main/java/com.mine.test/mine/q53.py
@@ -0,0 +1,14 @@
+
+
+
+
+def max_subarray(A):
+    max_ending_here = max_so_far = A[0]
+    for x in A[1:]:
+        max_ending_here = max(x, max_ending_here + x)
+        max_so_far = max(max_so_far, max_ending_here)
+    return max_so_far
+
+
+if __name__ == '__main__':
+    print(max_subarray([-6, 4, -2, 3, -2]))
