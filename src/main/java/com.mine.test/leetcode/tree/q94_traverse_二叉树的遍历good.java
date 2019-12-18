d212f0c32866a505352d89fbbf9bc598cb7d0f34
刘剑 <liujian@17shanyuan.com>
2019-12-18 11:55:55 +0800

    good

--- src/main/java/com.mine.test/leetcode/tree/q94_traverse_二叉树的遍历good.java
+++ src/main/java/com.mine.test/leetcode/tree/q94_traverse_二叉树的遍历good.java
@@ -29,7 +29,7 @@ import java.util.*;
 链接：https://leetcode-cn.com/problems/binary-tree-inorder-traversal
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  **/
-public class q94_traverse {
+public class q94_traverse_二叉树的遍历good {
 
     public static void main(String[] args) {
 
