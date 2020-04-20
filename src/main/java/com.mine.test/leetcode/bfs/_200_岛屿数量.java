fd59c257344b9b2e12c7242877ce48df2dfc8cf2
刘剑 <liujian@17shanyuan.com>
2020-04-20 16:57:19 +0800

    岛屿问题

---
+++ src/main/java/com.mine.test/leetcode/bfs/_200_岛屿数量.java
@@ -0,0 +1,47 @@
+package com.mine.test.leetcode.bfs;
+
+/**
+ *
+ * 给你一个由 '1'（陆地）和 '0'（水）组成的的二维网格，请你计算网格中岛屿的数量。
+ *
+ * 岛屿总是被水包围，并且每座岛屿只能由水平方向和/或竖直方向上相邻的陆地连接形成。
+ *
+ * 此外，你可以假设该网格的四条边均被水包围。
+ *
+ * 示例 1:
+ *
+ * 输入:
+ * 11110
+ * 11010
+ * 11000
+ * 00000
+ * 输出: 1
+ *
+ * 示例 2:
+ *
+ * 输入:
+ * 11000
+ * 11000
+ * 00100
+ * 00011
+ * 输出: 3
+ * 解释: 每座岛屿只能由水平和/或竖直方向上相邻的陆地连接而成。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/number-of-islands
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ *
+ * @author lj
+ * @createDate 2020/4/20 16:16
+ **/
+//在 LeetCode 中，「岛屿问题」是一个系列系列问题，比如：
+//
+//        LeetCode 463. Island Perimeter 岛屿的周长（Easy）
+//        LeetCode 695. Max Area of Island 岛屿的最大面积（Medium）
+//        LeetCode 827. Making A Large Island 填海造陆（Hard）
+
+
+//TODO :https://leetcode-cn.com/problems/number-of-islands/solution/dao-yu-lei-wen-ti-de-tong-yong-jie-fa-dfs-bian-li-/
+public class _200_岛屿数量 {
+}
