d4e816e2eaa0569ad5d417712bdba835fe43f2e1
刘剑 <liujian@17shanyuan.com>
2020-05-08 14:03:44 +0800

    good

--- src/main/java/com.mine.test/leetcode/q3.java
+++ src/main/java/com.mine.test/leetcode/q3.java
@@ -1,5 +1,8 @@
 package com.mine.test.leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author lj
  * @createDate 2019/8/1 17:35
@@ -35,7 +38,8 @@ public class q3 {
       //todo java char的范围为 -128 --127
     public static void main(String[] args) {
         Solution s = new Solution();
-        int r = s.lengthOfLongestSubstring("bbbabb");
+        //int r = lengthOfLongestSubstring2("abba");
+        int r = s.lengthOfLongestSubstring("abba");
         System.out.println(r);
 
 
@@ -106,5 +110,42 @@ public class q3 {
         }
     }
 
+    // https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/solution/hua-jie-suan-fa-3-wu-zhong-fu-zi-fu-de-zui-chang-z/
+    public int lengthOfLongestSubstring1(String s) {
+        int n = s.length(), ans = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        for (int end = 0, start = 0; end < n; end++) {
+            char alpha = s.charAt(end);
+            if (map.containsKey(alpha)) {
+                start = Math.max(map.get(alpha), start);
+            }
+            ans = Math.max(ans, end - start + 1);
+            map.put(s.charAt(end), end + 1);
+        }
+        return ans;
+    }
 
+
+    //TODO 思路  采用窗口管理，窗口内都是不重复的元素
+    public static int lengthOfLongestSubstring2(String s) {
+        if (s == null || s.length() == 0)return 0;
+        char[] chars = s.toCharArray();
+        int start = 0; //窗口开始
+        int end = 0; //窗口结束
+        int result = 0;
+        //以窗口结尾为索引进行遍历
+        //采用hash表存储每个字符的上一次出现的位置 +1；
+        int[] hash = new int[128];
+        for (  ; end < chars.length; end++) {
+            char c = chars[end];
+            //字符出现过，更新start位置
+            if (hash[c] > 0){
+                //避免出现abbbbbbbbbbbbba这种情况
+                start = Math.max(hash[c],start);
+            }
+            result = Math.max(result,end -start+1);
+            hash[c] = end+1;  //这里是为了取出来计算方便；
+        }
+        return result;
+    }
 }
