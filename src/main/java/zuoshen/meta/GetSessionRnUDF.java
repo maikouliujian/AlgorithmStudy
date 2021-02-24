8f5331401c5dd49b4d2eeb5f993845add0cac588
liujian <liujian1@yidian-inc.com>
2021-02-24 13:57:32 +0800

    矩阵

---
+++ src/main/java/zuoshen/meta/GetSessionRnUDF.java
@@ -0,0 +1,45 @@
+package zuoshen.meta;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ * @author : 刘剑
+ * @date : 2021/2/23 2:45 下午
+ * @description
+ */
+public class GetSessionRnUDF {
+
+    public int evaluate(int rn, ArrayList<Integer> rn_set) throws Exception{
+        int ret=1;
+        if(rn_set==null || rn_set.isEmpty()){
+            ret=1;
+        } else {
+            Collections.sort(rn_set);
+            for(int i=rn_set.size();i>=1;i--){
+                if(rn >= rn_set.get(i - 1)){
+                    ret = i+1;
+                    break;
+                }
+            }
+        }
+        return ret;
+    }
+
+    public static void main(String[] args) throws Exception{
+        ArrayList list = new ArrayList<>();
+        list.add(2);
+        list.add(4);
+        list.add(5);
+        list.add(7);
+
+        System.out.println(new GetSessionRnUDF().evaluate(1,list));
+        System.out.println(new GetSessionRnUDF().evaluate(2,list));
+        System.out.println(new GetSessionRnUDF().evaluate(3,list));
+        System.out.println(new GetSessionRnUDF().evaluate(4,list));
+        System.out.println(new GetSessionRnUDF().evaluate(5,list));
+        System.out.println(new GetSessionRnUDF().evaluate(6,list));
+        System.out.println(new GetSessionRnUDF().evaluate(7,list));
+
+    }
+}
--- src/main/java/zuoshen/矩阵问题.java
+++ src/main/java/zuoshen/矩阵问题.java
@@ -15,10 +15,15 @@ public class 矩阵问题 {
      * 3）原地旋转正方形矩阵
      */
     public static void main(String[] args) {
-        int[][] matrix = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 } };
-        printMatrixZigZag(matrix);
+        int[][] matrix = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 },{13,14,15,16}};
+        //printMatrixZigZag(matrix);
         System.out.println("------");
-        printMatrixZigZag_(matrix);
+        //printMatrixZigZag_(matrix);
+//        printMatrix(matrix);
+//        rotate(matrix);
+//        printMatrix(matrix);
+
+        spiralOrderPrint(matrix);
     }
 
     //====================================================
@@ -86,6 +91,95 @@ public class 矩阵问题 {
             //以d为基准点
             while (dR>=rR){
                 System.out.print(matrix[dR--][dC++] + " ");
+            }
+        }
+
+    }
+    /***
+     * 3）原地旋转正方形矩阵
+     */
+    //todo 将矩阵分组进行交换，抓住四个定点位置
+   //====================================================
+    public static void rotate(int[][] matrix) {
+        //定义四个控制变量来描述四个定点的坐标
+        int a = 0,b = 0;
+        int c = matrix.length -1,d = matrix[0].length-1;
+        //则四个定点位置为：(a,b) (a,d) (c,d) (c,b)
+        //定义分层
+        while (a < c){
+            rotateEdge(matrix,a++,b++,c--,d--);
+        }
+    }
+
+    private static void rotateEdge(int[][] matrix, int a, int b, int c, int d) {
+        //分组
+        int cnt = c - a -1;
+        for (int i = 0; i <= cnt; i++) {
+            int tmp = matrix[a][b+i];
+            matrix[a][b+i] = matrix[c-i][b];
+            matrix[c-i][b] = matrix[c][d-i];
+            matrix[c][d-i] = matrix[a+i][d];
+            matrix[a+i][d] = tmp;
+        }
+    }
+
+    public static void printMatrix(int[][] matrix) {
+        for (int i = 0; i != matrix.length; i++) {
+            for (int j = 0; j != matrix[0].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    //======================================================
+    /**
+     * 2）转圈打印矩阵
+     * todo 思路：定义两个基准点，开始旋转打印；
+     * todo 注意特殊情况：最后只剩下一行或者一列的情况；
+     */
+    public static void spiralOrderPrint(int[][] matrix) {
+
+        //t点
+        int tR = 0;
+        int tC = 0;
+        //d点
+        int dR = matrix.length - 1;
+        int dC = matrix[0].length - 1;
+        while (tR <= dR && tC <= dC) {
+            printEdge(matrix, tR++, tC++, dR--, dC--);
+
+        }
+    }
+
+    private static void printEdge(int[][] matrix, int tR, int tC, int dR, int dC) {
+        //todo 判断特殊情况
+        //最后只剩一行
+        if (tR == dR){
+            for (int i = tC; i <= dC; i++) {
+                System.out.print(matrix[tR][i]+" ");
+            }
+        }else if (tC == dC){
+            for (int i = tR; i <= dR; i++) {
+                System.out.print(matrix[i][tC]+" ");
+            }
+
+        }else {
+            //最后只剩一列
+
+            //以t点为基准点进行打印
+            int curR =tR,curC =tC;
+            while (curC < dC){
+                System.out.print(matrix[tR][curC++]+" ");
+            }
+            while (curR < dR){
+                System.out.print(matrix[curR++][dC]+" ");
+            }
+            while (curC > tC){
+                System.out.print(matrix[dR][curC--]+" ");
+            }
+            while (curR > tR){
+                System.out.print(matrix[curR--][tC]+" ");
             }
         }
 
